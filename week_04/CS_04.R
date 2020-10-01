11f262de9d5c412fa26776454bc04b38791e8080
Weishan Bai <weishanb@buffalo.edu>
2020-09-30 22:58:57 -0400

    0930

--- week_04/CS_04.R
+++ week_04/CS_04.R
@@ -1,10 +1,14 @@
 library(tidyverse)
 library(nycflights13)
 library(dbplyr)
-flights%>%left_join(airports,c("dest" = "faa"))%>%arrange(desc(distance))%>%slice(1L)%>%select(name)
+flights%>%
+  left_join(airports,c("dest" = "faa"))%>%
+  arrange(desc(distance))%>%
+  slice(1L)%>%
+  select(name)
 
-flights%>%left_join(airports,c("dest" = "faa"))%>%select(origin,dest,name)
-flights%>%left_join(airports,c("origin" = "faa"))%>%select(origin,dest,name)
+#flights%>%left_join(airports,c("dest" = "faa"))%>%select(origin,dest,name)
+#flights%>%left_join(airports,c("origin" = "faa"))%>%select(origin,dest,name)
 
                 
                 
