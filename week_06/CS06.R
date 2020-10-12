c92e89ed0aacd8c0dd8045331498d5e241d10186
Weishan Bai <weishanb@buffalo.edu>
2020-10-12 01:04:00 -0400

    1012

---
+++ week_06/CS06.R
@@ -0,0 +1,24 @@
+library(raster)
+library(sp)
+library(spData)
+library(tidyverse)
+library(sf)
+
+data(world)
+tmax_monthly <- getData(name = "worldclim", var="tmax", res=10)
+
+new_world<-world%>%filter(name_long != 'Antarctica')%>%as('Spatial')
+#str(tmax_monthly)
+#plot(tmax_monthly)
+
+tmax_annual<-max(tmax_monthly)
+names(tmax_annual)<-'tmax'
+
+max_country<-raster::extract(tmax_annual,new_world,fun=max,na.rm=T, small=T, sp=T)%>%st_as_sf()
+
+
+max_country%>%ggplot()+geom_sf(aes(fill=tmax/10))+
+  scale_fill_viridis_c(name="Annual\nMaximum\nTemperature (C)")+
+  theme(legend.position = 'bottom')
+
+max_country%>%as.data.frame%>%mutate(tmax=tmax/10)%>%group_by(`continent`)%>%top_n(1,`tmax`)%>%select(name_long,continent,tmax)%>%arrange(desc(tmax))
