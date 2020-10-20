3b3d7dc86498097d1a995e8a00a9cf772f72659b
Weishan Bai <weishanb@buffalo.edu>
2020-10-20 14:18:16 -0400

    1020

--- week_07/CS_07_nocomments.R
+++ week_07/CS_07_nocomments.R
@@ -3,8 +3,9 @@ library(spData)
 library(reprex)
 data(world)
 
-ggplot(world,aes(x=gdpPercap, y=continent, color=continent))+
-   geom_density(alpha=0.5,color=F)
+ggplot(world,aes(x=gdpPercap, group=continent, fill=continent))+
+   geom_density(alpha=0.5,color=F)+
+  theme(legend.position="bottom")
 
 reprex(
   {
